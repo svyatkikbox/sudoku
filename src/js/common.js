de74cf8fee75404cbe1b430a5225e4df1512b12a
svyatkikbox <svyatkikbox@gmail.com>
2019-09-03 18:04:15 +0300

    cell class

--- src/js/common.js
+++ src/js/common.js
@@ -1,22 +1,24 @@
+import Cell from './components/Cell'
+
 const randomInteger = (min, max) => {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
-}
+};
 
 function MakeMatrix() {
   const matrix = [];
-  
+
   while (matrix.length !== 9) {
     const row = [];
-    const range = [1,2,3,4,5,6,7,8,9];
-  
+    const range = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
     while (row.length !== 9) {
       const randIndex = randomInteger(0, range.length - 1);
 
       row.push(range[randIndex]);
-      range.splice(randIndex,1);
+      range.splice(randIndex, 1);
     }
-    
+
     matrix.push(row);
   }
 
@@ -33,26 +35,28 @@ function ShowMatrix(matrix, parent) {
     for (let j = 0; j < row.length; j++) {
       const number = row[j];
       const span = document.createElement('span');
-      
+
       if (j === 2 || j === 5) {
         span.classList.add('vertical');
       }
-      
+
       if (i === 2 || i === 5) {
         span.classList.add('horizontal');
       }
-    
+
       span.classList.add('cell');
       span.innerHTML = number;
       div.append(span);
-      
     }
-    
+
     document.querySelector(parent).append(div);
   }
-
 }
 
 const matrix = MakeMatrix();
 
 ShowMatrix(matrix, '.board');
+
+const cell = new Cell();
+
+document.body.appendChild(cell);
--- src/js/components/Cell.js
+++ src/js/components/Cell.js
@@ -1,8 +1,7 @@
 export default class Cell extends HTMLElement {
-  constructor() {
-    super();
+  connectedCallback() {
+    console.log('added');
   }
-
 }
 
 customElements.define('matrix-cell', Cell);
