bab2af9eca19149861bcb8a12b16243134ad083f
svyatkikbox <svyatkikbox@gmail.com>
2019-09-10 00:10:45 +0300

    clear non siutable

--- src/js/common.js
+++ src/js/common.js
@@ -1,57 +1,11 @@
 import MakeMatrix from './utils/MakeMatrix';
-import MakeMatrix2 from './utils/MakeMatrix2';
 import ShowMatrix from './utils/ShowMatrix';
 import { uniqInSector, uniqInCol, uniqInRow } from './utils/Search';
-// import FilterColsMatrix from './utils/FilterColsMatrix';
-// import FilterSquaresMatrix from './utils/FilterSquaresMatrix';
 
 //Создание матрицы с неповторяющимися цифрами в строке (!только в строке)
 const originalMatrix = MakeMatrix();
-// const matrixCopy = JSON.parse(JSON.stringify(originalMatrix));
-
-// console.log('=======================================');
-// console.log('Копия исходной матрицы');
-// console.log(matrixCopy);
-
-// Очищение от дублей внутри секторов 3х3
-// let filteredMatrix = FilterSquaresMatrix(originalMatrix);
-
-// console.log('=======================================');
-// console.log('очищение от дублей внутри секторов');
-// console.log(filteredMatrix);
-
-// Фильтрация матрицы по колонкам от дублей
-// filteredMatrix = FilterColsMatrix(originalMatrix);
-// console.log('=======================================');
-// console.log('Очищенные от дублей колонки матрицы');
-// console.log(filteredMatrix);
 
 ShowMatrix(originalMatrix, '.board');
-
-// console.log(uniqInSector({
-//   matrix: originalMatrix, 
-//   number: 4, 
-//   iSectorStart: 6, 
-//   jSectorStart: 6, 
-//   iSectorEnd: 8, 
-//   jSectorEnd: 8, 
-//   i: 7, 
-//   j: 7
-// }));
-
-// console.log(uniqInRow({ 
-//   matrix: originalMatrix, 
-//   number: 4, 
-//   i: 7, 
-//   j: 7 
-// }));
-
-// console.log(uniqInCol({ 
-//   matrix: originalMatrix, 
-//   number: 4, 
-//   i: 7, 
-//   j: 7 
-// }));
 
 const matrix2 = MakeMatrix2();
 console.log(matrix2);
--- src/js/utils/MakeMatrix.js
+++ src/js/utils/MakeMatrix.js
@@ -1,21 +1,70 @@
 import randomInteger from './RandomInt';
+import { uniqInSector, uniqInCol, uniqInRow } from './Search';
 
-export default function MakeMatrix() {
-  const matrix = [];
+export default () => {
+  // Главный массив, в котором лежат все строки
+  const MATRIX = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+  ];
 
-  while (matrix.length !== 9) {
-    const row = [];
-    const range = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //  В цикле формируется матрица построчно 
+  for (let i = 0; i < 9; i++) {
+    // Для каждой строки формируется массив с числами от 1 до 9
+    // из массива рандомно будут выбираться числа при помощи RandomInt
+    // После каждого удачного заполнения ячейки в строке, будет удаляться соответствующий элемент массива
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    while (row.length !== 9) {
-      const randIndex = randomInteger(0, range.length - 1);
+    for (let j = 0; j < 9; j++) {
+      // В условиях проверяется нахождение итерируемого элемента матрицы
+      // на принадлежность одному из 9 секторов 3х3 в порядке слева направо, сверху вниз
 
-      row.push(range[randIndex]);
-      range.splice(randIndex, 1);
+      // Выбираем из массива numbers рандомный элемент, который будем прогонять через проверки на правила судоку
+      if (i < 3 && j < 3) { // 1 sector
+
+      }
+
+      if (i < 3 && (j >= 3 && j < 6)) { // 2 sector
+
+      }
+
+      if (i < 3 && j >= 6) { // 3 sector
+
+      }
+
+      if ((i >= 3 && i < 6) && j < 3) { // 4 sector
+
+      }
+
+      if ((i >= 3 && i < 6) && (j >= 3 && j < 6)) { // 5 sector
+
+      }
+
+      if ((i >= 3 && i < 6) && j >= 6) { // 6 sector
+
+      }
+
+      if (i >= 6 && j < 3) { // 7 sector
+
+      }
+
+      if (i >= 6 && (j >= 3 && j < 6)) { // 8 sector
+
+      }
+
+      if (i >= 6 && j >= 6) { // 9 sector
+
+      }
     }
-
-    matrix.push(row);
   }
 
-  return matrix;
+  return MATRIX;
+
 }
