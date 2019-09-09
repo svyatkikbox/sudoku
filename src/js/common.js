3a270d5b6e320f1299e8b897f5ab3966844555b2
svyatkikbox <svyatkikbox@gmail.com>
2019-09-09 18:49:23 +0300

    new Search douplicates functions

--- src/js/common.js
+++ src/js/common.js
@@ -1,45 +1,36 @@
 import MakeMatrix from './utils/MakeMatrix';
+import MakeMatrix2 from './utils/MakeMatrix2';
 import ShowMatrix from './utils/ShowMatrix';
-import FilterColsMatrix from './utils/FilterColsMatrix';
-import FilterSquaresMatrix from './utils/FilterSquaresMatrix';
+import { uniqInSector, uniqInCol, uniqInRow } from './utils/Search';
+// import FilterColsMatrix from './utils/FilterColsMatrix';
+// import FilterSquaresMatrix from './utils/FilterSquaresMatrix';
 
 //Создание матрицы с неповторяющимися цифрами в строке (!только в строке)
 const originalMatrix = MakeMatrix();
-
 const matrixCopy = JSON.parse(JSON.stringify(originalMatrix));
-console.log('=======================================');
-console.log('Копия исходной матрицы');
-console.log(matrixCopy);
 
+// console.log('=======================================');
+// console.log('Копия исходной матрицы');
+// console.log(matrixCopy);
 
 // Очищение от дублей внутри секторов 3х3
-let filteredMatrix = FilterSquaresMatrix(originalMatrix);
-console.log('=======================================');
-console.log('очищение от дублей внутри секторов');
-console.log(filteredMatrix);
+// let filteredMatrix = FilterSquaresMatrix(originalMatrix);
 
-// Фильтрация матрицы по колонкам от дублей
-filteredMatrix = FilterColsMatrix(originalMatrix);
-console.log('=======================================');
-console.log('Очищенные от дублей колонки матрицы');
-console.log(filteredMatrix);
-
-
-
-
-
-
-
-// // Фильтрация матрицы по колонкам от дублей
-// let filteredMatrix = FilterColsMatrix(originalMatrix);
-// console.log('=======================================');
-// console.log('Очищенные от дублей колонки матрицы');
-// console.log(filteredMatrix);
-
-// // Очищение от дублей внутри секторов 3х3
-// filteredMatrix = FilterSquaresMatrix(filteredMatrix);
 // console.log('=======================================');
 // console.log('очищение от дублей внутри секторов');
 // console.log(filteredMatrix);
 
-ShowMatrix(filteredMatrix, '.board');
+// Фильтрация матрицы по колонкам от дублей
+// filteredMatrix = FilterColsMatrix(originalMatrix);
+// console.log('=======================================');
+// console.log('Очищенные от дублей колонки матрицы');
+// console.log(filteredMatrix);
+
+ShowMatrix(originalMatrix, '.board');
+
+console.log(uniqInSector({ matrix: originalMatrix, number: 7, iSector: 0, jSector: 0, i: 2, j: 2 }));
+console.log(uniqInRow({ matrix: originalMatrix, number: 7, i: 2, j: 2 }));
+console.log(uniqInCol({ matrix: originalMatrix, number: 7, i: 2, j: 2 }));
+
+const matrix2 = MakeMatrix2();
+console.log(matrix2);
---
+++ src/js/utils/Search.js
@@ -0,0 +1,45 @@
+// number - число, которое надо проверить на уникальность
+// iSector - i координата сектора (строка)
+// jSector - j координата сектора (столбец)
+// i, j - координаты элемента матрицы, для которого происходит поиск значения
+
+export const uniqInSector = ({ matrix, number, iSector, jSector, i, j }) => {
+  console.log('поиск в секторе значений равных', number);
+  console.log('для заполнения ячейки', i, ':', j);
+  for (let x = iSector; x <= i; x++) {
+    for (let y = jSector; y <= j; y++) {
+      console.log('проверяемый элемент равен', matrix[x][y]);
+      if (matrix[x][y] === number) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+export const uniqInRow = ({ matrix, number, i, j }) => {
+  console.log('поиск в строке значений равных', number);
+  console.log('для заполнения ячейки', i, ':', j);
+  for (let x = 0; x <= j; x++) {
+    console.log('проверяемый элемент равен', matrix[i][x]);
+    if (matrix[i][x] === number) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export const uniqInCol = ({ matrix, number, i, j }) => {
+  console.log('поиск в столбце значений равных', number);
+  console.log('для заполнения ячейки', i, ':', j);
+  for (let y = 0; y <= i; y++) {
+    console.log('проверяемый элемент равен', matrix[y][j]);
+    if (matrix[y][j] === number) {
+      return false;
+    }
+  }
+
+  return true;
+}
