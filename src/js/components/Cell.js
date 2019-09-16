c955d805e7ad37a69ed0c4100821dc0568b8450c
svyatkikbox <svyatkikbox@gmail.com>
2019-09-16 17:52:06 +0300

    numbers panel checking quiz cells with original matrix

--- src/js/components/Cell.js
+++ src/js/components/Cell.js
@@ -5,23 +5,74 @@ export default class Cell extends HTMLElement {
     this.classList = props.classList;
     this.innerText = props.innerText;
 
-    if (this.innerText === '?') {
-      this.style.color = 'salmon';
+    const ClearColored = (color) => {
+      const cells = document.querySelectorAll('matrix-cell');
+
+      cells.forEach(cell => {
+        cell.classList.remove(color);
+      });
+    }
+
+    if (this.innerText === '') { // скрытые ячейки помечаем классом
+      this.classList.add('quiz');
+
+      this.addEventListener('click', () => {
+
+        if (!this.classList.contains('active')) {
+          const activeQuiz = document.querySelectorAll('.cell.quiz.active');
+
+          activeQuiz.forEach(quiz => {
+            quiz.classList.remove('active');
+          });
+
+          this.classList.add('active');
+
+        } else { // повторный клик по активной ячейке снимает статус
+          this.classList.remove('active');
+        }
+
+      });
     }
 
     this.id = props.id;
 
-    this.onclick = () => {
-      console.log('this.innerText:', this.innerText);
-      console.log('this.id:', this.id);
+    this.addEventListener('click', () => {
+      const [i, j] = [...this.id]; // разбиваем id для получения координат ячейки
+      const cellNumber = this.innerText; // получаем ее содержимое
+
+      if (!this.classList.contains('colored') && !this.classList.contains('quiz')) { // если ячейка не покрашена и не скрытая
+        const colored = document.querySelectorAll('.colored');
+
+        if (colored) { // перед этим очищаем окрашенные ранее
+          colored.forEach(coloredCell => {
+            coloredCell.classList.remove('colored');
+          });
+        }
+
+        const similar = document.querySelectorAll('matrix-cell');
+
+        similar.forEach(cell => {
+          if (cell.innerText === cellNumber) {
+            cell.classList.add('colored');
+          }
+        });
+
+      } else { // при клике на уже окрашенные, убираем выделение
+        const colored = document.querySelectorAll('.colored');
+
+        colored.forEach(coloredCell => {
+          coloredCell.classList.remove('colored');
+        });
+      }
+
+      console.log('cellNumber:', cellNumber);
+      console.log('id:', this.id);
       console.log('i:', i);
       console.log('j:', j);
-      const [i, j] = [...this.id];
-    }
+    });
+
   }
 
-  connectedCallback() {
-  }
 }
 
 customElements.define('matrix-cell', Cell);
--- src/js/utils/Play.js
+++ src/js/utils/Play.js
@@ -5,24 +5,24 @@ import ShowMatrix from './ShowMatrix';
 export default function Play() {
   // Проверка ? существуют ли уже сохраненные настройки игры
   let settings = localStorage.getItem('sudoku-settings') ? JSON.parse(localStorage.getItem('sudoku-settings')) : 0;
-  
-  const menu = document.querySelector('.menu');
-  const toggleMenu = document.querySelector('.menu__toggle');
-  const levels = menu.querySelectorAll('.menu__btn');
-  
+
+  const menu = document.querySelector('.menu'); // главное меню с выбором сложности
+  const toggleMenu = document.querySelector('.menu__toggle'); // гамбургер
+  const levels = menu.querySelectorAll('.menu__btn'); // кнопки сложности
+
   // Каждая кнопка задает сложность игры
   // Определяя кол-во скрытых полей на доске
   for (const level of levels) {
     level.onclick = () => {
       const difficulty = parseInt(level.value);
-      
+
       // Сохраняем выбор пользователя
       settings.difficulty = difficulty;
       localStorage.setItem('sudoku-settings', JSON.stringify(settings));
-      
+
       // Скрываем меню
       menu.classList.toggle('show');
-      
+
       // Настройки сформированы, заново запускаем игру, чтобы произошел рендер доски
       Play();
     }
@@ -33,18 +33,19 @@ export default function Play() {
   if (!settings) {
     // Формируем объект с настройками игры
     settings = {};
-    
+
     // Показываем игроку меню
     menu.classList.toggle('show');
   } else {
     // Если повторный, то генерируем доску с учетом существующих настроек
     settings.difficulty = parseInt(settings.difficulty);
-    
-    const Matrix = MakeMatrix();
-    const copyMatrix = JSON.parse(JSON.stringify(Matrix));
 
-    HideCells(Matrix, settings);
-    ShowMatrix(Matrix, '.board');
+    const Matrix = MakeMatrix(); // главная матрица
+    const numbers = document.querySelectorAll('.number'); // нижняя панель с цифрами для ввода
+    const copyMatrix = JSON.parse(JSON.stringify(Matrix)); // копия матрицы для контроля игры
+
+    HideCells(Matrix, settings); // удаляет из матрицы кол-во ячеек, исходя из выбора сложности
+    ShowMatrix(Matrix, '.board'); // рисуется матрица на доске
 
     toggleMenu.classList.remove('active'); // так как страница не перезагружается, то кнопка "залипает" (остается активной)
     toggleMenu.classList.add('show'); // при первом запуске игры гамбурге скрыт, при повторном надо его показать
@@ -55,6 +56,27 @@ export default function Play() {
       menu.classList.toggle('show');
     }
 
+    for (const number of numbers) { // цифры для вставки в пустые ячейки
+      number.onclick = () => {
+        const activeQuiz = document.querySelector('.cell.quiz.active');
+
+        if (activeQuiz) {
+          const numberContent = number.innerText;
+          const [quizMatrixPositionX, quizMatrixPositionY] = [...activeQuiz.id];
+
+          console.log('i ', quizMatrixPositionX);
+          console.log('j ', quizMatrixPositionY);
+          console.log(numberContent);
+
+          activeQuiz.innerText = numberContent;
+
+          console.log('copyMatrix:', copyMatrix);
+          console.log(copyMatrix[quizMatrixPositionX][quizMatrixPositionY]);
+        } else {
+          console.log('выберите ячейку для вставки значения');
+        }
+      }
+    }
   }
 
 }
